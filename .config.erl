40917d374b0fd8c8f5f76a4959498bbc18cc4aed
Bikram Chatterjee <chatterjee@bluewin.ch>
2018-04-27 17:09:31 +0200

    dummy template

--- .config.erl
+++ .config.erl
@@ -1,1 +1,7 @@
-#{}.
+#{tns => "(DESCRIPTION=(CONNECT_TIMEOUT=4)(TRANSPORT_CONNECT_TIMEOUT=3) (ENABLE=BROKEN)(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=_host_)(PORT=_port_))(ADDRESS=(PROTOCOL=TCP)(HOST=_host_)(PORT=_port_)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=_service_name_)))",
+  user => "_user_",
+  password => "_password_",
+  host => "_host_",
+  port => _port_,
+  service_name => "_service_name_"
+}.
